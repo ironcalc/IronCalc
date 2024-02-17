36e1a73ffa8c64dcc95d96efaeb2ecb6b0e17ff2
Nicolás Hatcher <nicolas@theuniverse.today>
2024-02-17 17:54:44 +0100

    FIX: Lint issues

--- xlsx/src/lib.rs
+++ xlsx/src/lib.rs
@@ -1,22 +1,22 @@
 //! # IronCalc - Core API documentation
-//! 
-//! This technical API documentation in aimed at developers who want to develop bindings for a different language, 
+//!
+//! This technical API documentation in aimed at developers who want to develop bindings for a different language,
 //! build a UI based on the engine or just use the library in a Rust program
-//! 
+//!
 //! ## Usage
-//! 
+//!
 //! Add the dependency in `Cargo.toml`:
-//! 
+//!
 //! ```toml
 //! [dependencies]
 //! ironcalc = { git = "https://github.com/ironcalc/IronCalc", version = "0.1.0"}
 //! ```
-//! 
+//!
 //! <small> until version 0.5.0 you should use the git dependencies as stated </small>
-//! 
+//!
 //! A simple example:
-//! 
-//! 
+//!
+//!
 //! ```rust
 //! use ironcalc::{
 //! base::{expressions::utils::number_to_column, model::Model},
@@ -47,13 +47,12 @@
 //! Ok(())
 //! }
 //! ```
-//! 
+//!
 //! You can then just:
-//! 
+//!
 //! ```bash
 //! cargo run
 //! ```
-
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/ironcalc/ironcalc/main/assets/logo.png",
