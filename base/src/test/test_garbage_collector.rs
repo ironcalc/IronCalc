254a2f2defc6c559334f99c27a21ebbe024caade
fosdick.io <67963637+fosdickio@users.noreply.github.com>
2024-03-20 16:17:23 -0400

    Refactoring base/src/test/test_garbage_collector.rs.

--- base/src/test/test_garbage_collector.rs
+++ base/src/test/test_garbage_collector.rs
@@ -1,28 +1,7 @@
 use super::util::new_empty_model;
 
 #[test]
-fn shared_strings_one_reference() {
-    let mut model = new_empty_model();
-
-    // Set an arbitrary string in a cell
-    model._set("A1", "Hello");
-    assert_eq!(model.shared_strings.len(), 1);
-
-    // Calling the garbage collector should not do anything
-    model.garbage_collector().unwrap();
-    assert_eq!(model.shared_strings.len(), 1);
-
-    // Deleting the cell will leave the string in model.shared_strings
-    model.delete_cell(0, 1, 1).unwrap();
-    assert_eq!(model.shared_strings.len(), 1);
-
-    // The garbage collector should now remove the reference
-    model.garbage_collector().unwrap();
-    assert_eq!(model.shared_strings.len(), 0);
-}
-
-#[test]
-fn shared_strings_multiple_references() {
+fn shared_strings() {
     let mut model = new_empty_model();
 
     // Set an arbitrary string in a cell
@@ -30,15 +9,23 @@ fn shared_strings_multiple_references() {
     model._set("A2", "Hello");
     assert_eq!(model.shared_strings.len(), 1);
 
-    // Calling the garbage collector should not do anything
+    // Calling the garbage collector should not do anything since A1 and A2 reference the same string
     model.garbage_collector().unwrap();
     assert_eq!(model.shared_strings.len(), 1);
 
-    // Deleting the cell will leave the string in model.shared_strings
+    // Deleting a cell should leave the string in model.shared_strings because the GC hasn't run yet
     model.delete_cell(0, 1, 1).unwrap();
     assert_eq!(model.shared_strings.len(), 1);
 
-    // The garbage collector will leave the string in model.shared_strings since A2 is still referencing it
+    // The garbage collector should leave the string in model.shared_strings since A2 is still referencing it
     model.garbage_collector().unwrap();
     assert_eq!(model.shared_strings.len(), 1);
+
+    // Deleting a cell should leave the string in model.shared_strings because the GC hasn't run yet
+    model.delete_cell(0, 2, 1).unwrap();
+    assert_eq!(model.shared_strings.len(), 1);
+
+    // The garbage collector should remove the reference now that A1 and A2 are gone
+    model.garbage_collector().unwrap();
+    assert_eq!(model.shared_strings.len(), 0);
 }
