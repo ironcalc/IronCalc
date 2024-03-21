e9fc41541b6e60d66430db68802cf9bdecf378c0
Nicolás Hatcher <nicolas@theuniverse.today>
2024-03-21 23:07:07 +0100

    FIX: adds test

--- base/src/mock_time.rs
+++ base/src/mock_time.rs
@@ -20,3 +20,18 @@ pub fn get_milliseconds_since_epoch() -> i64 {
 pub fn set_mock_time(time: i64) {
     MOCK_TIME.with(|cell| *cell.borrow_mut() = time);
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::mock_time::MOCK_TIME;
+
+    use super::get_milliseconds_since_epoch;
+
+    #[test]
+    fn mock_time() {
+        let t = get_milliseconds_since_epoch();
+        assert_eq!(t, 1667906008578);
+
+        MOCK_TIME.with_borrow(|v| assert_eq!(*v, 1667906008578));
+    }
+}
