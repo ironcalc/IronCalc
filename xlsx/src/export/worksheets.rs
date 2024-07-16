a75856ca5c179c1725da5f29a0dcb9f99419fa84
varuntumbe <varuntumbe1@gmail.com>
2024-07-16 09:15:17 +0530

    addressing review comments : taking reference instead of cloning

--- xlsx/src/export/worksheets.rs
+++ xlsx/src/export/worksheets.rs
@@ -243,7 +243,7 @@ pub(crate) fn get_worksheet_xml(
     }
     let sheet_data = sheet_data_str.join("");
 
-    for merge_cell_ref in worksheet.merge_cells.clone() {
+    for merge_cell_ref in &worksheet.merge_cells {
         merged_cells_str.push(format!("<mergeCell ref=\"{merge_cell_ref}\"/>"))
     }
     let merged_cells_count = merged_cells_str.len();
