fc0cd0f0b965197f20d9e661ce699df8667ab834
Nicolás Hatcher <nicolas@theuniverse.today>
2024-10-13 11:36:00 +0200

    FIX: Set default row height to 22
    This matches the line height. So far a magic number

--- base/src/constants.rs
+++ base/src/constants.rs
@@ -3,7 +3,7 @@
 /// A column width of Excel value `w` will result in `w * COLUMN_WIDTH_FACTOR` pixels
 /// Note that these constants are inlined
 pub(crate) const DEFAULT_COLUMN_WIDTH: f64 = 100.0;
-pub(crate) const DEFAULT_ROW_HEIGHT: f64 = 21.0;
+pub(crate) const DEFAULT_ROW_HEIGHT: f64 = 22.0;
 pub(crate) const COLUMN_WIDTH_FACTOR: f64 = 12.0;
 pub(crate) const ROW_HEIGHT_FACTOR: f64 = 2.0;
 pub(crate) const DEFAULT_WINDOW_HEIGH: i64 = 600;
