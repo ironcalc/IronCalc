e130f784fd724fa1b41ebd418b0caf307a641481
Bruno Carvalhal <hello@brunocarvalhal.com>
2024-11-13 09:03:20 +0100

    add missing test case for multiple arguments in UNICODE

--- base/src/test/test_fn_unicode.rs
+++ base/src/test/test_fn_unicode.rs
@@ -55,7 +55,9 @@ fn test_error_cases() {
 fn wrong_number_of_arguments() {
     let mut model = new_empty_model();
     model._set("A1", "=UNICODE()");
+    model._set("A2", "=UNICODE(\"B\",\"A\")");
     model.evaluate();
 
     assert_eq!(model._get_text("A1"), *"#ERROR!");
+    assert_eq!(model._get_text("A2"), *"#ERROR!");
 }
