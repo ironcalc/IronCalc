d70ab85396e42ec2794442f927b955fd12e34901
Nicolás Hatcher <nicolas@theuniverse.today>
2024-11-27 21:30:41 +0100

    FIX: Adds some more tests fro FORMULATEXT

--- base/src/test/test_fn_formulatext.rs
+++ base/src/test/test_fn_formulatext.rs
@@ -8,10 +8,40 @@ fn simple_cases() {}
 #[test]
 fn wrong_number_of_arguments() {
     let mut model = new_empty_model();
-    model._set("A1", "=UNICODE()");
-    model._set("A2", "=UNICODE(\"B\",\"A\")");
+    model._set("A1", "=FORMULATEXT()");
+    model._set("A2", "=FORMULATEXT(\"B\",\"A\")");
     model.evaluate();
 
     assert_eq!(model._get_text("A1"), *"#ERROR!");
     assert_eq!(model._get_text("A2"), *"#ERROR!");
+}
+
+#[test]
+fn multi_sheet_ref() {
+    let mut model = new_empty_model();
+    model.new_sheet();
+    model._set("A1", "=FORMULATEXT(Sheet1!A1:Sheet2!A1)");
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"#ERROR!");
+}
+
+#[test]
+fn implicit_intersection() {
+    let mut model = new_empty_model();
+    model._set("A1", "=FORMULATEXT(C1:C2)");
+    model._set("A2", "=FORMULATEXT(D1:E1)");
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"#ERROR!");
+    assert_eq!(model._get_text("A2"), *"#ERROR!");
+}
+
+#[test]
+fn non_reference() {
+    let mut model = new_empty_model();
+    model._set("A1", "=FORMULATEXT(42)");
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"#ERROR!");
 }
