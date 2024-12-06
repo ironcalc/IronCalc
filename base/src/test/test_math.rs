0e577b1f9488573117f8d217ed5b2ebd57f172ea
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2024-12-06 21:31:13 +0100

    Implement SIGN()

--- base/src/test/test_math.rs
+++ base/src/test/test_math.rs
@@ -27,3 +27,31 @@ fn test_fn_sqrtpi_arguments() {
     assert_eq!(model._get_text("A1"), *"#ERROR!");
     assert_eq!(model._get_text("A2"), *"#ERROR!");
 }
+
+#[test]
+fn test_fn_sign_arguments() {
+    let mut model = new_empty_model();
+    model._set("A1", "=SIGN(-42)");
+    model._set("A2", "=SIGN(13)");
+    model._set("A3", "=SIGN(0)");
+    model._set("A4", "=SIGN()");
+    model._set("A5", "=SIGN('')");
+    model._set("A6", "=SIGN('Hello')");
+    model._set("A7", "=SIGN(1, 2)");
+    model._set("A8", "=SIGN(B8)");
+    model._set("A9", "=SIGN(4-4)");
+    model._set("A10", "=SIGN(-0.000001)");
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"-1");
+    assert_eq!(model._get_text("A2"), *"1");
+    assert_eq!(model._get_text("A3"), *"0");
+    assert_eq!(model._get_text("A4"), *"#ERROR!");
+    assert_eq!(model._get_text("A5"), *"#ERROR!");
+    assert_eq!(model._get_text("A6"), *"#ERROR!");
+    assert_eq!(model._get_text("A7"), *"#ERROR!");
+    assert_eq!(model._get_text("A8"), *"0");
+    assert_eq!(model._get_text("A9"), *"0");
+    assert_eq!(model._get_text("A10"), *"-1");
+}
