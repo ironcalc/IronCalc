0ebac5446de30e0155f05bfe002accec571d4bea
Gian Hancock <gian.hancock@outlook.com>
2025-01-09 21:52:06 +1100

    Add additional SUM function tests

--- base/src/test/test_fn_sum.rs
+++ base/src/test/test_fn_sum.rs
@@ -17,3 +17,112 @@ fn test_fn_sum_arguments() {
     assert_eq!(model._get_text("A3"), *"1");
     assert_eq!(model._get_text("A4"), *"4");
 }
+
+#[test]
+fn test_fn_sum_text_converted_to_number() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM("1")"#);
+    model._set("A2", r#"=SUM("1e2")"#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"1");
+    assert_eq!(model._get_text("A2"), *"100");
+}
+
+#[test]
+fn test_fn_sum_invalid_text() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM("a")"#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"#VALUE!");
+}
+
+#[test]
+fn test_fn_sum_text_in_range_not_converted() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(B1:D1)"#);
+    model._set("B1", r#"="100""#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"0");
+}
+
+#[test]
+fn test_fn_sum_text_in_reference_not_converted() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(B1)"#);
+    model._set("B1", r#"="100""#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"0");
+}
+
+#[test]
+fn test_fn_sum_text_in_indirect_reference_not_converted() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(INDIRECT("B1"))"#);
+    model._set("B1", r#"="100""#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"0");
+}
+
+#[test]
+fn test_fn_sum_text_in_indirect_reference() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(INDIRECT("B1"))"#);
+    model._set("B1", r#"100"#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"100");
+}
+
+#[test]
+fn test_fn_sum_invalid_text_in_range() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(B1:D1)"#);
+    model._set("B1", "a");
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"0");
+}
+
+#[test]
+fn test_fn_sum_invalid_text_in_reference() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(B1)"#);
+    model._set("B1", r#"a"#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"0");
+}
+
+#[test]
+fn test_fn_sum_boolean_values_converted() {
+    let mut model = new_empty_model();
+
+    model._set("A1", r#"=SUM(TRUE)"#);
+    model._set("A2", r#"=SUM(FALSE)"#);
+
+    model.evaluate();
+
+    assert_eq!(model._get_text("A1"), *"1");
+    assert_eq!(model._get_text("A2"), *"0");
+}
