c8ae835bbeee6cbb3986d3b1ea84a8f69e079024
Nicolás Hatcher Andrés <nicolas@theuniverse.today>
2025-11-01 11:23:29 +0100

    UPDATE: Adds unit tests for DEGREES and RADIANS (#495)

--- base/src/test/mod.rs
+++ base/src/test/mod.rs
@@ -9,6 +9,7 @@ mod test_currency;
 mod test_date_and_time;
 mod test_datedif_leap_month_end;
 mod test_days360_month_end;
+mod test_degrees_radians;
 mod test_error_propagation;
 mod test_fn_average;
 mod test_fn_averageifs;
