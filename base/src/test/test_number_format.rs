dc49afa2c3fb143658d064c69bc31e59ee95a268
Nicolás Hatcher <nicolas@theuniverse.today>
2025-11-19 21:50:16 +0100

    FIX: Format numbers a tad better
    I still think there is some way to go, but this is closer to Excel

--- base/src/test/test_number_format.rs
+++ base/src/test/test_number_format.rs
@@ -9,6 +9,15 @@ fn test_simple_format() {
 }
 
 #[test]
+fn test_maximum_zeros() {
+    let formatted = format_number(1.0 / 3.0, "#,##0.0000000000000000000", "en");
+    assert_eq!(formatted.text, "0.3333333333333330000".to_string());
+
+    let formatted = format_number(1234.0 + 1.0 / 3.0, "#,##0.0000000000000000000", "en");
+    assert_eq!(formatted.text, "1,234.3333333333300000000".to_string());
+}
+
+#[test]
 #[ignore = "not yet implemented"]
 fn test_wrong_locale() {
     let formatted = format_number(2.3, "General", "ens");
