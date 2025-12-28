fc2f163129fa17ba94de3bfaf30994c27498a76d
aex42 <tableful-cozy-shy@duck.com>
2025-12-28 03:37:21 +0000

    refactor: Move sequence detection logic to a new module

--- base/src/user_model/common.rs
+++ base/src/user_model/common.rs
@@ -23,7 +23,11 @@ use crate::user_model::history::{
     ColumnData, Diff, DiffList, DiffType, History, QueueDiffs, RowData,
 };
 
-use super::border_utils::is_max_border;
+use super::{
+    border_utils::is_max_border,
+    sequence_detector::{IntegerProgressionDetector, SequenceDetector},
+};
+
 /// Data for the clipboard
 pub type ClipboardData = HashMap<i32, HashMap<i32, ClipboardCell>>;
 
@@ -62,49 +66,6 @@ pub enum BorderType {
 pub struct BorderArea {
     pub(crate) item: BorderItem,
     pub(crate) r#type: BorderType,
-}
-
-trait SequenceDetector {
-    fn detect(&self, values: &[String]) -> Option<ArithmeticProgression>;
-}
-
-struct IntegerProgressionDetector;
-
-#[derive(Copy, Clone)]
-struct ArithmeticProgression {
-    last: i64,
-    step: i64,
-}
-
-impl ArithmeticProgression {
-    fn next(&self, i: usize) -> String {
-        (self.last + self.step * (i as i64 + 1)).to_string()
-    }
-}
-
-impl SequenceDetector for IntegerProgressionDetector {
-    fn detect(&self, values: &[String]) -> Option<ArithmeticProgression> {
-        values
-            .iter()
-            .map(|s| s.parse::<i64>())
-            .collect::<Result<Vec<_>, _>>()
-            .ok()
-            .filter(|nums| nums.len() >= 2)
-            .and_then(|numbers| {
-                let step = numbers[1] - numbers[0];
-                if step == 0 {
-                    return None;
-                }
-                let is_progression = numbers.windows(2).all(|w| w[1] - w[0] == step);
-
-                if is_progression {
-                    let last = numbers[numbers.len() - 1];
-                    Some(ArithmeticProgression { last, step })
-                } else {
-                    None
-                }
-            })
-    }
 }
 
 fn boolean(value: &str) -> Result<bool, String> {
