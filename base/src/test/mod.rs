db094ceed07acb7e0286e450b21e8d853e5f7164
Elsa <elsaminsut@gmail.com>
2026-01-19 00:12:14 +0100

    fix: reference in module tree, remove unnecessary comment

--- base/src/test/mod.rs
+++ base/src/test/mod.rs
@@ -7,6 +7,8 @@ mod test_cell_clear_contents;
 mod test_circular_references;
 mod test_column_width;
 mod test_criteria;
+mod test_currency;
+mod test_database;
 mod test_date_and_time;
 mod test_datedif_leap_month_end;
 mod test_days360_month_end;
