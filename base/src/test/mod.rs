638b14153a75ef90b723bfef50f28f3919dacb22
Elsa <elsaminsut@gmail.com>
2026-01-19 01:01:21 +0100

    fix: remove test_currency from mod.rs

--- base/src/test/mod.rs
+++ base/src/test/mod.rs
@@ -7,7 +7,6 @@ mod test_cell_clear_contents;
 mod test_circular_references;
 mod test_column_width;
 mod test_criteria;
-mod test_currency;
 mod test_database;
 mod test_date_and_time;
 mod test_datedif_leap_month_end;
