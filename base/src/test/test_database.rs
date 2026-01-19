0a09895cdc1862d0dd1c19f02c4b12f617b51cc8
Elsa <elsaminsut@gmail.com>
2026-01-19 19:48:57 +0100

    fix: lint issues

--- base/src/test/test_database.rs
+++ base/src/test/test_database.rs
@@ -2,7 +2,7 @@
 
 use crate::test::util::new_empty_model;
 
-#[test] 
+#[test]
 fn arguments() {
     let mut model = new_empty_model();
     model._set("A1", "=DMIN()");
@@ -19,7 +19,7 @@ fn arguments() {
     model._set("A10", "=DAVERAGE(2)");
     model._set("A11", "=DAVERAGE(1, 2)");
     model._set("A12", "=DAVERAGE(1, 2, 3, 4)");
-    
+
     model._set("A13", "=DSUM()");
     model._set("A14", "=DSUM(2)");
     model._set("A15", "=DSUM(1, 2)");
@@ -28,14 +28,12 @@ fn arguments() {
     model._set("A17", "=DCOUNT()");
     model._set("A18", "=DCOUNT(2)");
     model._set("A19", "=DCOUNT(1, 2)");
-    model._set("A20", "=DCOUNT(1, 2, 3, 4)");    
+    model._set("A20", "=DCOUNT(1, 2, 3, 4)");
 
     model._set("A21", "=DGET()");
     model._set("A22", "=DGET(2)");
     model._set("A23", "=DGET(1, 2)");
     model._set("A24", "=DGET(1, 2, 3, 4)");
-
-    
 
     model.evaluate();
 
@@ -50,12 +48,12 @@ fn arguments() {
     assert_eq!(model._get_text("A8"), *"#ERROR!");
 
     assert_eq!(model._get_text("A9"), *"#ERROR!");
-    assert_eq!(model._get_text("A10"), *"#ERROR!"); 
+    assert_eq!(model._get_text("A10"), *"#ERROR!");
     assert_eq!(model._get_text("A11"), *"#ERROR!");
     assert_eq!(model._get_text("A12"), *"#ERROR!");
 
     assert_eq!(model._get_text("A13"), *"#ERROR!");
-    assert_eq!(model._get_text("A14"), *"#ERROR!"); 
+    assert_eq!(model._get_text("A14"), *"#ERROR!");
     assert_eq!(model._get_text("A15"), *"#ERROR!");
     assert_eq!(model._get_text("A16"), *"#ERROR!");
 
