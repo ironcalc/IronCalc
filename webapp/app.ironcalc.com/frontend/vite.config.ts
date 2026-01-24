2286456a6fac60da8d747e02eba2288c0afa7da5
Jaap van der Plas <jvdplas@gmail.com>
2026-01-24 07:24:27 +0100

    workbook: don't bundle dependencies, only build ES module
    this allows emotion theme to pick up outside context

--- webapp/app.ironcalc.com/frontend/vite.config.ts
+++ webapp/app.ironcalc.com/frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import pkg from './package.json';
+import workbookPkg from '../../IronCalc/package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +13,7 @@ export default defineConfig({
       allow: ['../../../'],
     },
   },
+  resolve: {
+    dedupe: Object.keys(workbookPkg.peerDependencies).filter(dep => dep in pkg.dependencies)
+  }
 })
