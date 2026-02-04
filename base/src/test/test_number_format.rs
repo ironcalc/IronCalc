163fa3b0046deef38017929c61a3c61cde89532b
rezat-c <moon6478@duck.com>
2026-02-04 13:52:39 +0000

    fix: treat leading comma as text

--- base/src/test/test_number_format.rs
+++ base/src/test/test_number_format.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::unwrap_used)]
 
 use crate::number_format::format_number;
+use crate::test::util::new_empty_model;
+use crate::UserModel;
 
 #[test]
 fn test_simple_format() {
@@ -15,6 +17,28 @@ fn test_maximum_zeros() {
 
     let formatted = format_number(1234.0 + 1.0 / 3.0, "#,##0.0000000000000000000", "en");
     assert_eq!(formatted.text, "1,234.3333333333300000000".to_string());
+}
+
+#[test]
+fn test_leading_comma_text() {
+    let model = new_empty_model();
+    let mut model = UserModel::from_model(model);
+    model.set_user_input(0, 1, 1, ",10").unwrap(); // A1
+    model.set_user_input(0, 2, 1, ",100").unwrap(); // A2
+    model.set_user_input(0, 3, 1, ",1000").unwrap(); // A3
+
+    assert_eq!(
+        model.get_formatted_cell_value(0, 1, 1),
+        Ok(",10".to_string())
+    );
+    assert_eq!(
+        model.get_formatted_cell_value(0, 2, 1),
+        Ok(",100".to_string())
+    );
+    assert_eq!(
+        model.get_formatted_cell_value(0, 3, 1),
+        Ok(",1000".to_string())
+    );
 }
 
 #[test]
