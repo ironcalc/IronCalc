21631e0ca95fdf937562441d1efaacb3dd0a079b
Nicolás Hatcher <nicolas@theuniverse.today>
2026-02-07 11:02:45 +0100

    FIX: Adds missing test

--- base/src/test/user_model/mod.rs
+++ base/src/test/user_model/mod.rs
@@ -10,6 +10,7 @@ mod test_defined_names;
 mod test_delete_row_column_formatting;
 mod test_diff_queue;
 mod test_evaluation;
+mod test_fn_formulatext;
 mod test_general;
 mod test_grid_lines;
 mod test_keyboard_navigation;
