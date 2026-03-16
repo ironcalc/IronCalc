de733db275285c6db8c3a49ac212c113a9bb1d20
Daniel Gonzalez Albo <danielglezalbo@gmail.com>
2026-03-16 17:51:46 +0100

    update: add a button component and its story

---
+++ webapp/IronCalc/.storybook/preview.tsx
@@ -0,0 +1,24 @@
+import type { Preview } from "@storybook/react";
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../src/theme";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
