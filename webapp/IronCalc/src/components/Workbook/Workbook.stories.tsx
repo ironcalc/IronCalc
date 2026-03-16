26fb9dae96695a55a77a2b0ef7271bcb449c8c97
Daniel Gonzalez Albo <danielglezalbo@gmail.com>
2026-03-16 17:34:10 +0100

    chore: move workbook stories to components/workbook

---
+++ webapp/IronCalc/src/components/Workbook/Workbook.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useEffect, useState } from "react";
+
+import { IronCalc, init, Model } from "../../index";
+
+function WorkbookWithInit() {
+  const [model, setModel] = useState<Model | null>(null);
+
+  useEffect(() => {
+    async function start() {
+      await init();
+      setModel(new Model("Workbook1", "en", "UTC", "en"));
+    }
+    start();
+  }, []);
+
+  if (!model) return <div>Loading...</div>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <IronCalc model={model} ref={null} />
+    </div>
+  );
+}
+
+const meta = {
+  title: "Example/Workbook",
+  component: WorkbookWithInit,
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof WorkbookWithInit>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
