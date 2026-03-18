bafd9f32e9de4875948b8d381204220ae3b7c766
Nicolás Hatcher <nicolas@theuniverse.today>
2026-03-18 15:23:22 +0100

    UPDATE[widget]: Update dependencies

--- webapp/IronCalc/vite.config.ts
+++ webapp/IronCalc/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       fileName: 'ironcalc',
       formats: ['es']
     },
-    rollupOptions: {
+    rolldownOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: [
