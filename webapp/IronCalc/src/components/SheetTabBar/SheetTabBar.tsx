7962649cc5a942d0396ea15820961b7904f8ad93
Nicolás Hatcher <nicolas@theuniverse.today>
2026-03-19 18:23:07 +0100

    FIX: Update SheetTabBar

--- webapp/IronCalc/src/components/SheetTabBar/SheetTabBar.tsx
+++ webapp/IronCalc/src/components/SheetTabBar/SheetTabBar.tsx
@@ -137,22 +137,21 @@ function SheetTabBar(props: SheetTabBarProps) {
 }
 
 // Note I have to specify the font-family in every component that can be considered stand-alone
-const Container = styled("div")`
-  display: flex;
-  flex-direction: row;
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
-  display: flex;
-  height: ${NAVIGATION_HEIGHT}px;
-  align-items: center;
-  padding: 0px;
-  font-family: Inter;
-  overflow: hidden;
-  background-color: ${theme.palette.common.white};
-  border-top: 1px solid ${theme.palette.grey["300"]};
-`;
+const Container = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  right: 0,
+  height: NAVIGATION_HEIGHT,
+  alignItems: "center",
+  padding: 0,
+  fontFamily: "Inter",
+  overflow: "hidden",
+  backgroundColor: theme.palette.common.white,
+  borderTop: `1px solid ${theme.palette.grey[300]}`,
+}));
 
 const Sheets = styled("div")`
   flex-grow: 2;
@@ -165,9 +164,9 @@ const Sheets = styled("div")`
   height: 100%;
 `;
 
-const SheetInner = styled("div")`
-  display: flex;
-`;
+const SheetInner = styled("div")({
+  display: "flex",
+});
 
 const LeftButtonsContainer = styled("div")`
   display: flex;
@@ -181,13 +180,13 @@ const LeftButtonsContainer = styled("div")`
   }
 `;
 
-const VerticalDivider = styled("div")`
-  height: 100%;
-  width: 0px;
-  @media (max-width: 769px) {
-    border-right: 1px solid ${theme.palette.grey["200"]};
-  }
-`;
+const VerticalDivider = styled("div")(({ theme }) => ({
+  height: "100%",
+  width: 0,
+  "@media (max-width: 769px)": {
+    borderRight: `1px solid ${theme.palette.grey[200]}`,
+  },
+}));
 
 const RightContainer = styled("div")`
   display: flex;
