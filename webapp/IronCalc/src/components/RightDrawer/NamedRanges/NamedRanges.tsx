a90a0826da6f96cd98572c551d9dccdd7de124a1
Nicolás Hatcher <nicolas@theuniverse.today>
2026-03-19 15:28:44 +0100

    FIX: Update NamedRanges

--- webapp/IronCalc/src/components/RightDrawer/NamedRanges/NamedRanges.tsx
+++ webapp/IronCalc/src/components/RightDrawer/NamedRanges/NamedRanges.tsx
@@ -10,7 +10,6 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { theme } from "../../../theme";
 import { Button } from "../../Button/Button";
 import { IconButton } from "../../Button/IconButton";
 import { parseRangeInSheet } from "../../Editor/util";
@@ -299,146 +298,150 @@ const Container = styled("div")({
   flexDirection: "column",
 });
 
-const Content = styled("div")({
+const Content = styled("div")(({ theme }) => ({
   flex: 1,
   color: theme.palette.grey[700],
-  lineHeight: "1.5",
+  lineHeight: 1.5,
   overflow: "auto",
-});
+}));
 
 const ListContainer = styled("div")({
   display: "flex",
   flexDirection: "column",
 });
 
-const ListItem = styled("div")<{ $isSelected: boolean }>(({ $isSelected }) => ({
-  display: "flex",
-  alignItems: "flex-start",
-  justifyContent: "space-between",
-  gap: "8px",
-  padding: "8px 12px",
-  cursor: "pointer",
-  minHeight: "40px",
-  boxSizing: "border-box",
-  borderBottom: `1px solid ${theme.palette.grey[200]}`,
-  paddingLeft: $isSelected ? "20px" : "12px",
-  transition: "all 0.2s ease-in-out",
-  borderLeft: $isSelected
-    ? `3px solid ${theme.palette.primary.main}`
-    : "3px solid transparent",
-  "&:hover": {
-    backgroundColor: theme.palette.grey[50],
-    paddingLeft: "20px",
-  },
-}));
+const ListItem = styled("div")<{ $isSelected: boolean }>(
+  ({ theme, $isSelected }) => ({
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    gap: 8,
+    padding: "8px 12px",
+    cursor: "pointer",
+    minHeight: 40,
+    boxSizing: "border-box",
+    borderBottom: `1px solid ${theme.palette.grey[200]}`,
+    paddingLeft: $isSelected ? 20 : 12,
+    transition: "all 0.2s ease-in-out",
+    borderLeft: $isSelected
+      ? `3px solid ${theme.palette.primary.main}`
+      : "3px solid transparent",
 
-const ListItemText = styled("div")({
-  fontSize: "12px",
+    "&:hover": {
+      backgroundColor: theme.palette.grey[50],
+      paddingLeft: 20,
+    },
+  }),
+);
+
+const ListItemText = styled("div")(({ theme }) => ({
+  fontSize: 12,
   color: theme.palette.common.black,
   fontFamily: theme.typography.fontFamily,
   flex: 1,
   display: "flex",
   flexDirection: "column",
   alignItems: "flex-start",
-  gap: "2px",
-});
+  gap: 2,
+}));
 
-const ScopeText = styled("span")({
-  fontSize: "12px",
+const ScopeText = styled("span")(({ theme }) => ({
+  fontSize: 12,
   color: theme.palette.common.black,
-});
+}));
 
-const FormulaText = styled("span")({
-  fontSize: "12px",
+const FormulaText = styled("span")(({ theme }) => ({
+  fontSize: 12,
   color: theme.palette.grey[600],
-});
+}));
 
-const NameText = styled("span")({
-  fontSize: "12px",
+const NameText = styled("span")(({ theme }) => ({
+  fontSize: 12,
   color: theme.palette.common.black,
   fontWeight: 600,
   wordBreak: "break-all",
   overflowWrap: "break-word",
-});
+}));
 
 const IconsWrapper = styled("div")({
   display: "flex",
   alignItems: "center",
-  gap: "2px",
+  gap: 2,
 });
 
-export const Footer = styled("div")`
-  padding: 8px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  font-size: 12px;
-  color: ${theme.palette.grey["600"]};
-  border-top: 1px solid ${theme.palette.grey["300"]};
-  gap: 8px;
-`;
+export const Footer = styled("div")(({ theme }) => ({
+  padding: 8,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  fontSize: 12,
+  color: theme.palette.grey[600],
+  borderTop: `1px solid ${theme.palette.grey[300]}`,
+  gap: 8,
+}));
 
-const Header = styled("div")({
-  height: "40px",
+const Header = styled("div")(({ theme }) => ({
+  height: 40,
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-end",
   padding: "0 8px",
   borderBottom: `1px solid ${theme.palette.grey[300]}`,
-});
+}));
 
 const HeaderTitle = styled("div")({
   width: "100%",
-  fontSize: "12px",
+  fontSize: 12,
 });
 
-const EditHeader = styled("div")({
-  height: "40px",
+const EditHeader = styled("div")(({ theme }) => ({
+  height: 40,
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
   padding: "0 8px",
-  gap: "8px",
+  gap: 8,
   borderBottom: `1px solid ${theme.palette.grey[300]}`,
-});
+}));
 
 const EditHeaderTitle = styled("div")({
   flex: 1,
-  fontSize: "12px",
+  fontSize: 12,
   fontWeight: 500,
 });
 
-const EmptyStateMessage = styled("div")`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  width: 100%;
-  height: 100%;
-  font-size: 12px;
-  color: ${theme.palette.grey["600"]};
-  font-family: "Inter";
-  z-index: 0;
-  margin: auto 0px;
-  position: relative;
-`;
+const EmptyStateMessage = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 8,
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  width: "100%",
+  height: "100%",
+  fontSize: 12,
+  color: theme.palette.grey[600],
+  fontFamily: "Inter",
+  zIndex: 0,
+  margin: "auto 0px",
+  position: "relative",
+}));
 
-const IconWrapper = styled("div")`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 36px;
-  height: 36px;
-  border-radius: 4px;
-  background-color: ${theme.palette.grey["100"]};
-  color: ${theme.palette.grey["600"]};
-  svg {
-    width: 16px;
-    height: 16px;
-    stroke-width: 2;
-  }
-`;
+const IconWrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: 36,
+  height: 36,
+  borderRadius: 4,
+  backgroundColor: theme.palette.grey[100],
+  color: theme.palette.grey[600],
+
+  "& svg": {
+    width: 16,
+    height: 16,
+    strokeWidth: 2,
+  },
+}));
 
 export default NamedRanges;
