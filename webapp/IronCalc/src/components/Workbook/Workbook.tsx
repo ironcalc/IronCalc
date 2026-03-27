aec27331a731fba115d28a0784f0c18501fd84a4
Nicolás Hatcher <nicolas@theuniverse.today>
2026-03-27 16:15:07 +0100

    UPDATE[Widget]: Remove MUI from Worksheet.tsx

--- webapp/IronCalc/src/components/Workbook/Workbook.tsx
+++ webapp/IronCalc/src/components/Workbook/Workbook.tsx
@@ -167,7 +167,9 @@ const Workbook = (props: { model: Model; workbookState: WorkbookState }) => {
     }
     workbookState.setCopyStyles(styles);
     // FIXME: This is so that the cursor indicates there are styles to be pasted
-    const el = rootRef.current?.getElementsByClassName("sheet-container")[0];
+    const el = rootRef.current?.getElementsByClassName(
+      "ic-worksheet-sheet-container",
+    )[0];
     if (el) {
       // Taken from lucide icons: <PaintRoller /> and rotated.
       const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-paint-roller" style="transform:rotate(-8deg)"><rect width="16" height="6" x="2" y="2" rx="2"></rect><path d="M10 16v-2a2 2 0 0 1 2-2h8a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2"></path><rect width="4" height="6" x="8" y="16" rx="1"></rect></svg>`;
@@ -334,7 +336,9 @@ const Workbook = (props: { model: Model; workbookState: WorkbookState }) => {
     onEscape: (): void => {
       workbookState.clearCutRange();
       workbookState.setCopyStyles(null);
-      const el = rootRef.current?.getElementsByClassName("sheet-container")[0];
+      const el = rootRef.current?.getElementsByClassName(
+        "ic-worksheet-sheet-container",
+      )[0];
       if (el) {
         (el as HTMLElement).style.cursor = "auto";
       }
