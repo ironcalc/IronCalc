f829d35254fdb910a9a7b4446aa472d213d15a2a
Nicolás Hatcher <nicolas@theuniverse.today>
2026-04-12 11:24:21 +0200

    UPDATE: Use build.rs to generate test files
    Also compile but not run examples to avoid the annoying xlsx artifacts
    
    Fixes a part of #648

---
+++ xlsx/tests/generated_tests.rs
@@ -0,0 +1,6 @@
+#![allow(clippy::unwrap_used)]
+#![allow(clippy::panic)]
+
+// Generated per-file tests
+// one #[test] per .xlsx file, produced by build.rs.
+include!(concat!(env!("OUT_DIR"), "/generated_tests.rs"));
