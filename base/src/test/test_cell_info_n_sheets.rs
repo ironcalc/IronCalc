5f1ce41e127eb510244c532a64c570809a083536
Elsa <elsaminsut@gmail.com>
2026-04-23 19:24:45 +0200

    feat: implement info function, add build.rs, fix test

--- base/src/test/test_cell_info_n_sheets.rs
+++ base/src/test/test_cell_info_n_sheets.rs
@@ -8,7 +8,7 @@ fn arguments() {
     model._set("A1", "=CELL(\"address\",A1)");
     model._set("A2", "=CELL()");
 
-    model._set("A3", "=INFO(\"system\")");
+    model._set("A3", "=INFO(\"recalc\")");
     model._set("A4", "=INFO()");
 
     model._set("A5", "=N(TRUE)");
@@ -23,7 +23,7 @@ fn arguments() {
     assert_eq!(model._get_text("A1"), *"$A$1");
     assert_eq!(model._get_text("A2"), *"#ERROR!");
 
-    assert_eq!(model._get_text("A3"), *"#N/IMPL!");
+    assert_eq!(model._get_text("A3"), *"Automatic");
     assert_eq!(model._get_text("A4"), *"#ERROR!");
 
     assert_eq!(model._get_text("A5"), *"1");
