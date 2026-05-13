375ca74ec6635041a4b42a71cbb647fb3f96bcfc
Stefan Wermelinger <wermelinger.stefan@gmail.com>
2026-05-13 11:08:10 +0200

    test: add additional test

--- base/src/test/test_fn_mmult.rs
+++ base/src/test/test_fn_mmult.rs
@@ -3,6 +3,32 @@
 use crate::test::util::new_empty_model;
 
 #[test]
+fn mmult_2x2() {
+    let mut model = new_empty_model();
+    // A = [[1, 2],
+    //      [3, 4]]
+    model._set("A1", "1");
+    model._set("B1", "2");
+    model._set("A2", "3");
+    model._set("B2", "4");
+    // B = [[5, 6],
+    //      [7, 8]]
+    model._set("D1", "5");
+    model._set("E1", "6");
+    model._set("D2", "7");
+    model._set("E2", "8");
+
+    model._set("G1", "=MMULT(A1:B2, D1:E2)");
+    model.evaluate();
+    // A*B = [[19, 22],
+    //        [43, 50]]
+    assert_eq!(model._get_text("G1"), "19");
+    assert_eq!(model._get_text("H1"), "22");
+    assert_eq!(model._get_text("G2"), "43");
+    assert_eq!(model._get_text("H2"), "50");
+}
+
+#[test]
 fn mmult_2x3_times_3x2() {
     let mut model = new_empty_model();
     // A = [[1, 2, 3],
