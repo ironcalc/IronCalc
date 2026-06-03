849490b78b34646dca8954a0dcd6d8b741978760
rezat-c <moon6478@duck.com>
2026-06-03 01:06:14 +0000

    fix: use stand-alone date forms

--- generate_locale/src/dates.rs
+++ generate_locale/src/dates.rs
@@ -8,7 +8,8 @@ use crate::constants::{DateFormats, Dates, LOCAL_TYPE};
 
 #[derive(Serialize, Deserialize, Encode)]
 struct CaGCalendarsFormat {
-    format: HashMap<String, HashMap<String, String>>,
+    #[serde(rename = "stand-alone")]
+    stand_alone: HashMap<String, HashMap<String, String>>,
 }
 #[derive(Serialize, Deserialize, Encode)]
 struct CaGCalendarsII {
@@ -53,10 +54,10 @@ pub fn get_dates_formatting(cldr_dir: &str, locale_id: &str) -> Result<Dates, &'
     let ca_gregorian: CaGregorian =
         serde_json::from_str(&contents).or(Err("Failed parsing 'ca-gregorian' file"))?;
     let gregorian = &ca_gregorian.main[locale_id].dates.calendars.gregorian;
-    // See: http://cldr.unicode.org/translation/date-time-1/date-time-patterns
-    // for the difference between stand-alone and format. We will use only the format mode
-    let months_format = &gregorian.months.format;
-    let days_format = &gregorian.days.format;
+    // See: https://cldr.unicode.org/translation/date-time/date-time-patterns
+    // for the difference between stand-alone and format. We will use only the stand-alone mode
+    let months_stand_alone = &gregorian.months.stand_alone;
+    let days_stand_alone = &gregorian.days.stand_alone;
 
     let mut day_names = vec![];
     let mut day_names_short = vec![];
@@ -69,15 +70,15 @@ pub fn get_dates_formatting(cldr_dir: &str, locale_id: &str) -> Result<Dates, &'
         "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
     ];
     for index in month_index {
-        months_letter.push(months_format["narrow"][index].to_owned());
-        months_short.push(months_format["abbreviated"][index].to_owned());
-        months.push(months_format["wide"][index].to_owned());
+        months_letter.push(months_stand_alone["narrow"][index].to_owned());
+        months_short.push(months_stand_alone["abbreviated"][index].to_owned());
+        months.push(months_stand_alone["wide"][index].to_owned());
     }
 
     let day_index = vec!["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
     for day in day_index {
-        day_names_short.push(days_format["abbreviated"][day].to_owned());
-        day_names.push(days_format["wide"][day].to_owned());
+        day_names_short.push(days_stand_alone["abbreviated"][day].to_owned());
+        day_names.push(days_stand_alone["wide"][day].to_owned());
     }
 
     Ok(Dates {
