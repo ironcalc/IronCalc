53a3ecf07e1f6142127fa01371b2fdc7d8dfdcef
Daniel Gonzalez Albo <danielglezalbo@gmail.com>
2026-06-30 14:56:19 +0200

    fix: font size

--- webapp/IronCalc/src/components/Editor/Editor.tsx
+++ webapp/IronCalc/src/components/Editor/Editor.tsx
@@ -207,7 +207,7 @@ const Editor = (options: EditorOptions) => {
           display: showEditor,
           background: "#FFF",
           fontFamily: "var(--palette-sheet-default-cell-font-family)",
-          fontSize: "13px",
+          fontSize: "12px",
         }}
       >
         <div
